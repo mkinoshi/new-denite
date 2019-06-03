42a634a3b916876b0a6a6fdb11d17dc339ec5d9f
makotokinoshita <mkinoshi12@gmail.com>
2019-06-02 23:34:50 -0700

    WIP trying to figure out how it works

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -72,6 +72,7 @@ class Default(object):
 
         self._result = []
         context['sources_queue'] = [sources]
+        context['sources_queue'].append([{'name': 'file', 'args': ['new']}])
         self._sources_history = []
         try:
             while context['sources_queue']:
@@ -128,7 +129,6 @@ class Default(object):
             self._context.update(context)
             self._context['sources'] = sources
             self._context['is_redraw'] = False
-            self._context['args'] = ['new']
             self._current_mode = context['mode']
             self._is_multi = len(sources) > 1
 

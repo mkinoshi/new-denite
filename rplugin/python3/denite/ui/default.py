8da3c064e6cdad2161814d6f45b7682fd0e375b4
makotokinoshita <mkinoshi12@gmail.com>
2019-06-02 23:09:30 -0700

    WIP trying to figure out how it works

--- rplugin/python3/denite/ui/default.py
+++ rplugin/python3/denite/ui/default.py
@@ -128,7 +128,7 @@ class Default(object):
             self._context.update(context)
             self._context['sources'] = sources
             self._context['is_redraw'] = False
-            self._context['args'] = 'new'
+            self._context['args'] = ['new']
             self._current_mode = context['mode']
             self._is_multi = len(sources) > 1
 
